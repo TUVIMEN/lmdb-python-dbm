4292c52f56be75004186bb4190d5e8538cb5120b
TUVIMEN <hexderm@gmail.com>
2026-01-16 18:42:03 +0100

    add support for dynamic inheritance

--- lmdbm/__init__.py
+++ lmdbm/__init__.py
@@ -1,7 +1,7 @@
 """Python DBM style wrapper around LMDB (Lightning Memory-Mapped Database)"""
 
-from .lmdbm import Lmdb, LmdbGzip, SizeError, GrowError, Error, open
+from .lmdbm import Lmdb, SizeError, GrowError, Error, open, LmdbJson, LmdbCompress
 
 __version__ = "0.0.6"
 
-__all__ = ["Lmdb", "LmdbGzip", "Error", "SizeError", "GrowError", "open", "__version__"]
+__all__ = ["Lmdb", "Error", "SizeError", "GrowError", "open", "LmdbJson", "LmdbCompress", "__version__"]
--- lmdbm/lmdbm.py
+++ lmdbm/lmdbm.py
@@ -1,8 +1,10 @@
 import logging
 from collections.abc import Mapping, MutableMapping
-from gzip import compress, decompress
+import gzip
 from pathlib import Path
 from typing import Any, Generic, Iterator, List, Optional, Tuple, TypeVar, Union, Callable
+from threading import Lock
+import json
 
 import lmdb
 from typing_extensions import Self
@@ -12,6 +14,8 @@ KeyT = TypeVar("KeyT")
 ValueT = TypeVar("ValueT")
 
 _DEFAULT = object()
+
+text_encoding = "utf8"
 
 
 class Error(Exception):
@@ -40,8 +44,8 @@ class MissingOk:
             return True
 
 
-def remove_lmdbm(file: str, missing_ok: bool = True) -> None:
-    base = Path(file)
+def remove_lmdbm(path: str, missing_ok: bool = True) -> None:
+    base = Path(path)
     with MissingOk(missing_ok):
         (base / "data.mdb").unlink()
     with MissingOk(missing_ok):
@@ -54,16 +58,29 @@ def to_bytes(value):
     if isinstance(value, bytes):
         return value
     elif isinstance(value, str):
-        return value.encode("utf8")
+        return value.encode(text_encoding)
 
     raise TypeError(value)
+
+
+def split_kwargs(keys, kwargs):
+    found = {}
+    other = {}
+
+    for i in kwargs:
+        if i in keys:
+            found[i] = kwargs[i]
+        else:
+            other[i] = kwargs[i]
+
+    return found, other
 
 
 class Lmdb(MutableMapping, Generic[KeyT, ValueT]):
     autogrow_error = "Failed to grow LMDB ({}). Is there enough disk space available?"
     autogrow_msg = "Grew database (%s) map size to %s"
 
-    def __init__(self, env: lmdb.Environment, autogrow: bool, logger: Callable) -> None:
+    def __init__(self, env: lmdb.Environment, autogrow: bool = True, logger: Callable = None, **kwargs) -> None:
         self.env = env
         self.autogrow = autogrow
         self.logger = logger
@@ -71,39 +88,71 @@ class Lmdb(MutableMapping, Generic[KeyT, ValueT]):
     @classmethod
     def open(
         cls,
-        file: str,
+        path: str,
         flag: str = "r",
         mode: int = 0o755,
         map_size: int = 2**20,
-        autogrow: bool = True,
-        logger: Callable = None,
         **kwargs,
     ) -> "Lmdb":
         """
-        Opens the database `file`.
+        Opens the database `path`.
         `flag`: r (read only, existing), w (read and write, existing),
                 a (read, write, create if not exists), n (read, write, overwrite existing)
                 c (read, write, create if not exists), n (read, write, overwrite existing)
-        `map_size`: Initial database size. Defaults to 2**20 (1MB).
         `autogrow`: Automatically grow the database size when `map_size` is exceeded.
                 WARNING: Set this to `False` for multi-process write access.
-        `**kwargs`: All other keyword arguments are passed through to `lmdb.open`.
+        `path`: Location of directory (if subdir=True) or file prefix to store the database.
+        `mode`: File creation mode.
+        `logger`: Function used for logging.
+        `map_size`: Maximum size database may grow to; used to size the memory mapping. Defaults to 2**20 (1MB). If database grows larger than map_size, an exception will be raised and the user must close and reopen Environment. On 64-bit there is no penalty for making this huge (say 1TB). Must be <2GB on 32-bit.
+            Note
+            The default map size is set low to encourage a crash, so users can figure out a good value before learning about this option too late.
+        `subdir`: If True, path refers to a subdirectory to store the data and lock files in, otherwise it refers to a filename prefix.
+        `metasync`: If False, flush system buffers to disk only once per transaction, omit the metadata flush. Defer that until the system flushes files to disk, or next commit or sync().
+            This optimization maintains database integrity, but a system crash may undo the last committed transaction. I.e. it preserves the ACI (atomicity, consistency, isolation) but not D (durability) database property.
+        `sync`: If False, don’t flush system buffers to disk when committing a transaction. This optimization means a system crash can corrupt the database or lose the last transactions if buffers are not yet flushed to disk.
+            The risk is governed by how often the system flushes dirty buffers to disk and how often sync() is called. However, if the filesystem preserves write order and writemap=False, transactions exhibit ACI (atomicity, consistency, isolation) properties and only lose D (durability). I.e. database integrity is maintained, but a system crash may undo the final transactions.
+            Note that sync=False, writemap=True leaves the system with no hint for when to write transactions to disk, unless sync() is called. map_async=True, writemap=True may be preferable.
+        `readahead`: If False, LMDB will disable the OS filesystem readahead mechanism, which may improve random read performance when a database is larger than RAM.
+        `writemap`: If True, use a writeable memory map unless readonly=True. This is faster and uses fewer mallocs, but loses protection from application bugs like wild pointer writes and other bad updates into the database. Incompatible with nested transactions.
+            Processes with and without writemap on the same environment do not cooperate well.
+        `meminit`: If False LMDB will not zero-initialize buffers prior to writing them to disk. This improves performance but may cause old heap data to be written saved in the unused portion of the buffer. Do not use this option if your application manipulates confidential data (e.g. plaintext passwords) in memory. This option is only meaningful when writemap=False; new pages are always zero-initialized when writemap=True.
+        `map_async`: When writemap=True, use asynchronous flushes to disk. As with sync=False, a system crash can then corrupt the database or lose the last transactions. Calling sync() ensures on-disk database integrity until next commit.
+        `max_readers`: Maximum number of simultaneous read transactions. Can only be set by the first process to open an environment, as it affects the size of the lock file and shared memory area. Attempts to simultaneously start more than this many read transactions will fail.
+        `max_spare_txns`: Read-only transactions to cache after becoming unused. Caching transactions avoids two allocations, one lock and linear scan of the shared environment per invocation of begin(), Transaction, get(), gets(), or cursor(). Should match the process’s maximum expected concurrent transactions (e.g. thread count).
         """
 
+        settings, args = split_kwargs(
+            {
+                "lock",
+                "sync",
+                "readahead",
+                "writemap",
+                "meminit",
+                "mode",
+                "subdir",
+                "metasync",
+                "map_async",
+                "max_readers",
+                "max_spare_txns",
+            },
+            kwargs,
+        )
+
         if flag == "r":  # Open existing database for reading only (default)
-            env = lmdb.open(file, map_size=map_size, max_dbs=1, readonly=True, create=False, mode=mode, **kwargs)
+            env = lmdb.open(path, map_size=map_size, max_dbs=1, readonly=True, create=False, mode=mode, **settings)
         elif flag == "w":  # Open existing database for reading and writing
-            env = lmdb.open(file, map_size=map_size, max_dbs=1, readonly=False, create=False, mode=mode, **kwargs)
+            env = lmdb.open(path, map_size=map_size, max_dbs=1, readonly=False, create=False, mode=mode, **settings)
         # what a retarded convention to name it 'c'
         elif flag == "a" or flag == "c":  # Open database for reading and writing, creating it if it doesn't exist
-            env = lmdb.open(file, map_size=map_size, max_dbs=1, readonly=False, create=True, mode=mode, **kwargs)
+            env = lmdb.open(path, map_size=map_size, max_dbs=1, readonly=False, create=True, mode=mode, **settings)
         elif flag == "n":  # Always create a new, empty database, open for reading and writing
-            remove_lmdbm(file)
-            env = lmdb.open(file, map_size=map_size, max_dbs=1, readonly=False, create=True, mode=mode, **kwargs)
+            remove_lmdbm(path)
+            env = lmdb.open(path, map_size=map_size, max_dbs=1, readonly=False, create=True, mode=mode, **settings)
         else:
             raise ValueError("Invalid flag")
 
-        return cls(env, autogrow, logger)
+        return cls(env, **args)
 
     @property
     def map_size(self) -> int:
@@ -249,18 +298,44 @@ class Lmdb(MutableMapping, Generic[KeyT, ValueT]):
         self.close()
 
 
-class LmdbGzip(Lmdb):
-    def __init__(self, env, autogrow: bool, compresslevel: int = 9):
-        Lmdb.__init__(self, env, autogrow)
-        self.compresslevel = compresslevel
+def LmdbJson(classtype=Lmdb):
+    class JsonLmdb(Lmdb):
+        def _pre_value(self, value):
+            return json.dumps(value, separators=(",", ":")).encode(text_encoding)
 
-    def _pre_value(self, value: ValueT) -> bytes:
-        value = Lmdb._pre_value(self, value)
-        return compress(value, self.compresslevel)
-
-    def _post_value(self, value: bytes) -> ValueT:
-        return decompress(value)
+        def _post_value(self, value):
+            return json.loads(value.decode(text_encoding))
 
 
-def open(file, flag="r", mode=0o755, **kwargs):
-    return Lmdb.open(file, flag, mode, **kwargs)
+def LmdbCompress(classtype=Lmdb):
+    class entity(classtype):
+        def __init__(
+            self, env, compressfunc=gzip.compress, decompressfunc=gzip.decompress, compresslevel: int = 9, **kwargs
+        ):
+            classtype.__init__(env, **kwargs)
+            self.compresslevel = compresslevel
+            self.compressfunc = compressfunc
+            self.decompressfunc = decompressfunc
+
+        def _pre_value(self, value: ValueT) -> bytes:
+            value = classtype._pre_value(value)
+            return self.compressfunc(value, self.compresslevel)
+
+        def _post_value(self, value: bytes) -> ValueT:
+            return self.decompressfunc(value)
+
+    return entity
+
+
+def chain(*args):
+    assert len(args) > 0
+    ret = Lmdb
+    for i in reversed(args):
+        ret = i(ret)
+    return ret
+
+
+def open(path, flag="r", mode=0o755, classtype=Lmdb, **kwargs):
+    if isinstance(classtype, list):
+        classtype = chain(classtype)
+    return classtype.open(path, flag, mode, **kwargs)
